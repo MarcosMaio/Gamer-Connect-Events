617eb7559c8e87d85b1af6327ff4d3a5af88a854
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-07 16:23:03 -0300

    project finished

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ export default defineConfig({
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: [...refreshPaths, "app/Livewire/**"],
-            build: {
-                outDir: "dist",
-            },
         }),
     ],
 });
